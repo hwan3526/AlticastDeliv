56b23d57e2e8b0895a42cd1a3f54924032b7fba5
hwan <34718901+Paradii@users.noreply.github.com>
2018-11-02 09:30:26 +0900

    get searchresult finished
    검색결과기능 완료. 리스트뷰에 점포명, 경도, 위도 순으로 나오게 만듦

--- app/src/main/java/com/example/wlghk/alticastdeliv/MainActivity.java
+++ app/src/main/java/com/example/wlghk/alticastdeliv/MainActivity.java
@@ -4,23 +4,30 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements Runnable{
-    ListView listView, listView2;
-    ArrayAdapter<String> adapter, adapter2;
+    ListView listView;
+    ArrayAdapter<String> adapter;
     TextView textView;
 
     String htmlsource;
     String[] name;
-    String[] addr;
+    String[] lon;
+    String[] lat;
 
     Handler handler = new Handler(){
         @Override
@@ -28,14 +35,12 @@ public class MainActivity extends AppCompatActivity implements Runnable{
             super.handleMessage(msg);
 
             getInformation();
-            textView.setText(htmlsource);
-            for(int i = 0;i < 10;i++){
-                adapter.add(name[i]);
-                adapter2.add(addr[i]);
-            }
+            //textView.setText(htmlsource);//사용된 HTML 소스가 보고 싶다면 사용
+            for(int i = 0;i < 30;i++)
+                adapter.add(name[i]+" "+lon[i]+" "+lat[i]);
 
             listView.setAdapter(adapter);
-            listView2.setAdapter(adapter2);
+
         }
     };
 
@@ -45,14 +50,14 @@ public class MainActivity extends AppCompatActivity implements Runnable{
         setContentView(R.layout.activity_main);
 
         listView = (ListView) findViewById(R.id.listView);
-        listView2 = (ListView) findViewById(R.id.listView2);
         textView = (TextView) findViewById(R.id.textView);
 
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1);
-        adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1);
 
-        name = new String[10];
-        addr = new String[10];
+        htmlsource = "";
+        name = new String[30];
+        lon = new String[30];
+        lat = new String[30];
 
         Thread th = new Thread(MainActivity.this);
         th.start();
@@ -60,88 +65,49 @@ public class MainActivity extends AppCompatActivity implements Runnable{
 
     @Override
     public void run(){
-        htmlsource = getHTML("피자");
+        getHTML("피자");
 
         handler.sendEmptyMessage(0);
     }
 
-    String getHTML(String keyword) {
-        String htmlCode = "";
+    void getHTML(String keyword) {
+        String str;
 
         try {
-            //URL url = new URL("https://www.google.com/");
-            URL url = new URL("https://map.naver.com/?query=" + /*현재 위치 주소*/ keyword);
-            InputStreamReader isr = new InputStreamReader(url.openStream());
+            URL url = new URL("https://m.map.naver.com/search2/search.nhn?query="+keyword+"&sm=clk&centerCoord=37.291859079115994:126.97775963708631#/list");
+            //또는 "https://m.map.naver.com/search2/search.nhn?query=" + /*현재 위치 주소*/ keyword + "&siteLocation=&queryRank=&type="
+            InputStreamReader isr = new InputStreamReader(url.openStream(),"UTF-8");
             BufferedReader br = new BufferedReader(isr);
 
-            String inputLine;
-            while ((inputLine = br.readLine()) != null)
-                htmlCode += inputLine;
+            while(!(str = br.readLine()).contains("var searchResult"));
+            htmlsource = str;
 
             br.close();
             isr.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        return htmlCode;
     }
 
     void getInformation(){
-        int start, end;
-        /*
-        start = htmlsource.indexOf("장소 리스트");
-        start = htmlsource.indexOf("<dt>", start);
-        end = htmlsource.indexOf( "a href", start);
-        for(int i = 0;i < 10;i++) {
-            name[i] = "";
-            addr[i] = "";
-            //점포명 추출
-            while(true) {
-                start = htmlsource.indexOf(">", end) + 1;
-                end = htmlsource.indexOf("<", start);
-                if("" == htmlsource.substring(start, end))
-                    continue;
-                else
-                    name[i] += htmlsource.substring(start, end);
+        int index=0, start, end;
 
-                if ("</a>" == htmlsource.substring(end, htmlsource.indexOf(">", end) + 1))
-                    break;
-            }
-
-            //주소 추출
-            end = htmlsource.indexOf( "addr", start);
-            start = htmlsource.indexOf(">", end) + 1;
-            end = htmlsource.indexOf("          ", start);//띄어쓰기 10칸?
-            addr[i] = htmlsource.substring(start, end);
-
-            start = htmlsource.indexOf("<dt>", end);
-            end = htmlsource.indexOf("a href", start);
+        for(int i = 0; i < 30; i++){
+            //점포명
+            index = htmlsource.indexOf("\"index\"", index);
+            start = (htmlsource.indexOf("\"name\"", index))+9;
+            end = htmlsource.indexOf("\"", start);
+            name[i] = htmlsource.substring(start, end);
+            //경도
+            start = (htmlsource.indexOf("\"lng\"", end))+8;
+            end = htmlsource.indexOf("\"", start);
+            lon[i] = htmlsource.substring(start, end);
+            //위도
+            start = (htmlsource.indexOf("\"lat\"", end))+8;
+            end = htmlsource.indexOf("\"", start);
+            lat[i] = htmlsource.substring(start, end);
+            index = end;
         }
-        */
-        fail();
-    }
-
-    void fail(){
-        name[0] = "1";
-        addr[0] = "서울";
-        name[1] = "2";
-        addr[1] = "대전";
-        name[2] = "3";
-        addr[2] = "대구";
-        name[3] = "4";
-        addr[3] = "부산";
-        name[4] = "5";
-        addr[4] = "광주";
-        name[5] = "6";
-        addr[5] = "인천";
-        name[6] = "7";
-        addr[6] = "용인";
-        name[7] = "8";
-        addr[7] = "부천";
-        name[8] = "9";
-        addr[8] = "수원";
-        name[9] = "10";
-        addr[9] = "구리";
     }
 }
